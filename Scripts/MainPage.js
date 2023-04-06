dbd0b7ade601258c21fb0e144a0d400abc1bb7bd
Na Wang <na.wang-8@student.manchester.ac.uk>
2023-04-06 15:13:03 +0100

    JS for listing page V1

--- Scripts/MainPage.js
+++ Scripts/MainPage.js
@@ -1,6 +1,56 @@
-Listings = [];
+let Listings = [];
 
 
 function LoadListings(){
-    return null;
+  /*
+    var AllJobs = {{totalJobs.values()}};
+    for(var Job:AllJobs){
+        let Date = {{Job.get_date()}};
+        let startTime = {{Job.get_startTime()}};
+        let endTime = {{Job.get_endTime()}};
+        let Location = {{Job.get_location()}};
+        let Description = {{job.get_description()}};
+    }
+
+    */
+
+
+
+
+    /*
+          <button onclick="buyHire( {{event.get_id()}} )">
+            GET JOB!
+          </button>
+
+          */
+         /*
+ListingsDiv = document.getElementById("Listings");
+Listing = document.createElement("div");
+Listing.class = "card";
+ListingDetails = document.createElement("div");
+ListingDetails.class = "card-details";
+ListingName = document.createElement("h1");
+Listing
+  
+          echo('<div class="card">');
+          echo('<div class="card-details">');
+          echo('<p class="text-title">'.$row['category_name'].'</p>');
+          echo('</div>');
+          echo('<button class="card-button" type="submit" name="categoryID" value="'.$row['ID'].'">');
+
+          echo($row['category_name']);
+          echo('</button></div>');
+
+
+          round_paragraph = document.getElementById("score-display");
+
+  round_paragraph.innerHTML = "";
+  round_text = document.createElement("div");
+  round_text.id = "round-text";
+  round_text.innerText = "Round: " + round;
+
+  round_paragraph.appendChild(round_text);
+}
+*/
+  return null;
 };
